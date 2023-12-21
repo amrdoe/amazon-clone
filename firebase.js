11eef725c3132188574bfc2d6cde2dcfc3737609
Amr Ibrahim Khudair <amrikhudair@outlook.com>
2023-12-21 16:06:35 +0200

    Add firbase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB96yavxC9CO_nceZAF4krjhzF9umj8ykA",
+  authDomain: "challenge-3aff8.firebaseapp.com",
+  projectId: "challenge-3aff8",
+  storageBucket: "challenge-3aff8.appspot.com",
+  messagingSenderId: "825984596086",
+  appId: "1:825984596086:web:2cf333dee713ca5edfc757",
+  measurementId: "G-E50LHB79CC"
+};
